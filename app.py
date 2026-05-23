69eca207bf3abd11de790717898b2fb063057c09
Jhana Dabu <jhanamareedabu@gmail.com>
2026-05-22 12:27:21 -1200

    Phase 1 setup

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+
+def home():
+
+    return """
+    NETSENTINEL RUNNING
+    """
+
+if __name__=="__main__":
+
+    app.run(
+        debug=True
+    )
